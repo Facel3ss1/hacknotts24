fd204b54b721474220d0c101d47a0adc53b4f5a2
Peter Medus <16763503+Facel3ss1@users.noreply.github.com>
2023-11-04 16:43:03 +0000

    Add SignalR Hub and basic console client

--- server/MovieApi/Program.cs
+++ server/MovieApi/Program.cs
@@ -1,11 +1,16 @@
+using MovieApi.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -21,5 +26,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<GameHub>("/GameHub");
 
 app.Run();
