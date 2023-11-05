2884ee91f38299dbb3f1198f96a6f710ec2de3ec
Charlie Baker <psycb9@nottingham.ac.uk>
2023-11-05 09:16:23 +0000

    Added round edges and title

--- client/app/src/main/java/com/example/movieapp/data/remote/Movie.kt
+++ client/app/src/main/java/com/example/movieapp/data/remote/Movie.kt
@@ -1,3 +1,11 @@
 package com.example.movieapp.data.remote
 
-data class Movie(val id: Int, val title: String, val releaseYear: Int?, val posterImageURL: String?)
+data class Movie(val id: Int, val title: String, val releaseYear: Int?, val posterImageURL: String?) {
+    override fun toString(): String {
+        return if (releaseYear != null) {
+            "$title ($releaseYear)"
+        } else {
+            title
+        }
+    }
+}
