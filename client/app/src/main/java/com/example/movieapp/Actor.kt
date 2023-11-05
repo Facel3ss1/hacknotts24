39401ba65414aa1be53c3ce2f047d2630a145de5
Charlie Baker <psycb9@nottingham.ac.uk>
2023-11-05 03:20:16 +0000

    Added actor and movie class

---
+++ client/app/src/main/java/com/example/movieapp/Actor.kt
@@ -0,0 +1,3 @@
+package com.example.movieapp
+
+data class Actor(val id: Int, val name: String, val profileImageURL: String?)
---
+++ client/app/src/main/java/com/example/movieapp/Movie.kt
@@ -0,0 +1,3 @@
+package com.example.movieapp
+
+data class Movie(val id: Int, val title: String, val releaseYear: Int?, val posterImageURL: String?)
